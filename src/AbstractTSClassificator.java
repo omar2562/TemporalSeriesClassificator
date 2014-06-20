15bdca453e13abe59684b3053da526001d2cff0f
omar2562 <omar2562@gmail.com>
2014-06-20 17:06:51 -0300

    Sakoe-Chiba band

--- src/AbstractTSClassificator.java
+++ src/AbstractTSClassificator.java
@@ -14,9 +14,12 @@ public abstract class AbstractTSClassificator<T extends MoveInterface> {
 
 	private int[][] results = new int[2][12];
 
+	private int sakoeChibaBand;
+
 	public AbstractTSClassificator() {
 		Arrays.fill(results[0], 0);
 		Arrays.fill(results[1], 0);
+		sakoeChibaBand = 100;
 	}
 
 	public void fillTrainingMoves(String trainingFile)
@@ -58,22 +61,24 @@ public abstract class AbstractTSClassificator<T extends MoveInterface> {
 		for (WiiMove<T> move : testMoveVector) {
 			approximateMove = new WiiMove<T>();
 			distanceMin = Float.MAX_VALUE;
-			//System.err.println("====== "+move.getWiiMoveType()+" ======");
+			// System.err.println("====== "+move.getWiiMoveType()+" ======");
 			for (WiiMove<T> trainTest : trainingMoveVector) {
 				matrix = new double[move.getMoveVector().size() + 1][trainTest
 						.getMoveVector().size() + 1];
-				fillEmptyMatrix();
-				fillBaseCase(move, trainTest);
+				// fillEmptyMatrix();
+				// fillBaseCase(move, trainTest);
+				fillMatrixMatrix(move, trainTest);
+				// printMatrix();
 				distance = dtw(move, move.getMoveVector().size(), trainTest,
 						trainTest.getMoveVector().size());
 				// printMatrix();
-				//System.out.println("calculado...."+ distance);
+				// System.out.println("calculado...."+ distance);
 				if (Math.abs(distance - distanceMin) < 0.000001) {
-					//System.err.println("equal " + trainTest.getWiiMoveType()
+					// System.err.println("equal " + trainTest.getWiiMoveType()
 					// + ","+approximateMove.getWiiMoveType());
 				}
 				if (distance < distanceMin) {
-					//System.err.println("diff " + trainTest.getWiiMoveType() +
+					// System.err.println("diff " + trainTest.getWiiMoveType() +
 					// ","+approximateMove.getWiiMoveType());
 					approximateMove = trainTest;
 					distanceMin = distance;
@@ -83,9 +88,9 @@ public abstract class AbstractTSClassificator<T extends MoveInterface> {
 			if (move.getWiiMoveType() != move.getWiiMoveTypeAprox()) {
 				falsePositive++;
 				results[1][move.getWiiMoveType() - 1]++;
-				//System.err.println("KO");
+				// System.err.println("KO");
 			} else {
-				//System.err.println("OK");
+				// System.err.println("OK");
 			}
 			// System.out.println("falso positivo:" + falsePositive
 			// +"  ---- tipo O: "+move.getWiiMoveType()+" N:"+
@@ -93,14 +98,46 @@ public abstract class AbstractTSClassificator<T extends MoveInterface> {
 		}
 	}
 
+	private void fillMatrixMatrix(WiiMove<T> move, WiiMove<T> trainTest) {
+		int minPossibleValue, maxPossibleValue;
+		int N = move.getMoveVector().size();
+		int M = trainTest.getMoveVector().size();
+		int T = (sakoeChibaBand * (N <= M ? N - 1 : M)) / 100;
+		Arrays.fill(matrix[0], Double.MAX_VALUE);
+		int negativeNumber;
+		for (int i = 1; i < matrix.length; i++) {
+			minPossibleValue = (int) Math.floor((1. * (M - T) / (N - T))
+					* (i - T));
+			minPossibleValue = minPossibleValue <= 0 ? 1 : minPossibleValue;
+			maxPossibleValue = (int) Math
+					.ceil((1. * (M - T) / (N - T)) * i + T);
+			maxPossibleValue = maxPossibleValue > M ? M : maxPossibleValue;
+			negativeNumber = 0;
+			for (int j = 0; j < matrix[0].length; j++) {
+				if (j < minPossibleValue || j > maxPossibleValue)
+					matrix[i][j] = Double.MAX_VALUE;
+				else {
+					matrix[i][j] = -1;
+					negativeNumber++;
+				}
+			}
+			if (negativeNumber == 0)
+				System.out.println("Matrix Incompleat:" + i + " ,type"
+						+ move.getWiiMoveType());
+		}
+		matrix[0][0] = 0;
+	}
+
 	private void fillBaseCase(WiiMove<T> a, WiiMove<T> b) {
 		T zero = (T) a.getMoveVector().firstElement().getZero();
 		matrix[0][0] = 0;
 		for (int i = 1; i < matrix.length; i++) {
-			matrix[i][0] = Double.MAX_VALUE;//a.getMoveVector().get(i - 1).difference(zero);
+			matrix[i][0] = Double.MAX_VALUE;// a.getMoveVector().get(i -
+											// 1).difference(zero);
 		}
 		for (int i = 1; i < matrix[0].length; i++) {
-			matrix[0][i] = Double.MAX_VALUE;//b.getMoveVector().get(i - 1).difference(zero);
+			matrix[0][i] = Double.MAX_VALUE;// b.getMoveVector().get(i -
+											// 1).difference(zero);
 		}
 	}
 
@@ -127,26 +164,35 @@ public abstract class AbstractTSClassificator<T extends MoveInterface> {
 	private void printMatrix() {
 		for (int i = 0; i < matrix.length; i++) {
 			for (int j = 0; j < matrix[0].length; j++) {
-				System.out.format("%06.3f |", matrix[i][j]);
+				if (matrix[i][j] != Double.MAX_VALUE)
+					System.out.format("%06.3f |", matrix[i][j]);
+				else
+					System.out.print("  inf  |");
 			}
 			System.out.println();
 		}
 	}
 
 	public void printResults() {
-		System.out.format("training: %d, test: %d%n",trainingMoveVector.size(),testMoveVector.size());
+		String outputHeadFormat = "result training:%d test:%d BandPrec:%d%n";
+		//outputHeadFormat = "training: %d, test: %d BandPrec: %d%n";
+		System.out.format(outputHeadFormat,
+				trainingMoveVector.size(), testMoveVector.size(),
+				sakoeChibaBand);
 		int falseP = 0, total = 0;
+		// String outputFormat =
+		// "type: %d ,false+: %d ,total: %d ,percentage: %.2f%n";
+		String outputFormat = "%d %d %d %.2f%n";
+		//String outputResultFormat = "general: ,false+: %d ,total: %d ,percentage: %.2f%n";
+		String outputResultFormat = "general %d %d %.2f%n";
 		for (int i = 0; i < results[0].length; i++) {
 			falseP += results[1][i];
 			total += results[0][i];
-			System.out.format(
-					"type: %d ,false+: %d ,total: %d ,percentage: %.2f%n",
-					i + 1, results[1][i], results[0][i], 100.0 * results[1][i]
-							/ results[0][i]);
+			System.out.format(outputFormat, i + 1, results[1][i],
+					results[0][i], 100.0 * results[1][i] / results[0][i]);
 		}
-		System.out.println("---------------------------------------------------");
 		System.out.format(
-				"general: ,false+: %d ,total: %d ,percentage: %.2f%n", falseP,
+				outputResultFormat, falseP,
 				total, 100.0 * falseP / total);
 	}
 
@@ -172,6 +218,14 @@ public abstract class AbstractTSClassificator<T extends MoveInterface> {
 
 	public double getAccertPercentage() {
 		return 100.0 * falsePositive / trainingMoveVector.size();
+	}
+
+	public int getSakoeChibaBand() {
+		return sakoeChibaBand;
+	}
+
+	public void setSakoeChibaBand(int sakoeChibaBand) {
+		this.sakoeChibaBand = sakoeChibaBand;
 	}
 
 }
